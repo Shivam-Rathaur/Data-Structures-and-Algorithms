42709f7ec20d97a6e4fe9ecce3a9c75e703c0a1e
Shivam Rathaur <rathoreshivam.iith@gmail.com>
2026-02-14 21:28:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (91.9%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.cpp
+++ 3340-check-balanced-string/3340-check-balanced-string.cpp
@@ -3,12 +3,9 @@ public:
     bool isBalanced(string num) {
         int evenSum = 0, oddSum = 0;
         for(int i=0 ; i<num.size() ; i++){
-            if(i%2==0){ //even indices
-                evenSum += num[i] - '0';
-            } else{ //odd indices
-                oddSum += num[i] - '0';
-            }
+            if(i%2==0) evenSum += num[i] - '0';
+            else oddSum += num[i] - '0';
         }
-        return evenSum == oddSum;
+        return evenSum==oddSum;
     }
 };
